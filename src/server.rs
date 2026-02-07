8fce375cb79fd3b309dd447eaa425ad1d6e19c93
2018wzh <wzh20070720@outlook.com>
2026-02-07 16:31:37 +0800

    [ipc] Improved performance by removing copy of MsgArgs

--- src/server.rs
+++ src/server.rs
@@ -4,7 +4,7 @@ use alloc::string::String;
 use glenda::cap::{CapPtr, Endpoint, Reply};
 use glenda::error::Error;
 use glenda::interface::{InitService, SystemService};
-use glenda::ipc::{Badge, MsgArgs, MsgFlags, MsgTag, UTCB};
+use glenda::ipc::{Badge, MsgFlags, MsgTag, UTCB};
 use glenda::protocol;
 
 impl SystemService for InitManager {
@@ -33,48 +33,44 @@ impl SystemService for InitManager {
             let utcb = unsafe { UTCB::get() };
             let msg_info = utcb.msg_tag;
             let badge = utcb.badge;
-            let label = msg_info.label();
-            let proto = msg_info.proto();
-            let flags = msg_info.flags();
-            let args = utcb.mrs_regs;
 
-            let res = self.dispatch(badge, label, proto, flags, args);
+            let res = self.dispatch(badge, msg_info);
             match res {
-                Ok(ret) => self.reply(
+                Ok(_) => self.reply(MsgTag::new(
                     protocol::GENERIC_PROTO,
                     protocol::generic::REPLY,
                     MsgFlags::OK,
-                    ret,
-                )?,
+                ))?,
                 Err(e) => match e {
                     Error::Success => {
                         continue;
                     }
-                    Error::HasCap => self.reply(
+                    Error::HasCap => self.reply(MsgTag::new(
                         protocol::GENERIC_PROTO,
                         protocol::generic::REPLY,
                         MsgFlags::OK | MsgFlags::HAS_CAP,
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                    )?,
-                    _ => self.reply(
-                        protocol::GENERIC_PROTO,
-                        protocol::generic::REPLY,
-                        MsgFlags::ERROR,
-                        [e as usize, 0, 0, 0, 0, 0, 0, 0],
-                    )?,
+                    ))?,
+                    _ => {
+                        let utcb = unsafe { UTCB::get() };
+                        utcb.mrs_regs[0] = e as usize;
+                        self.reply(MsgTag::new(
+                            protocol::GENERIC_PROTO,
+                            protocol::generic::REPLY,
+                            MsgFlags::ERROR,
+                        ))?
+                    }
                 },
             }
         }
         Ok(())
     }
-    fn dispatch(
-        &mut self,
-        badge: Badge,
-        label: usize,
-        proto: usize,
-        flags: MsgFlags,
-        msg: MsgArgs,
-    ) -> Result<MsgArgs, Error> {
+    fn dispatch(&mut self, badge: Badge, info: MsgTag) -> Result<(), Error> {
+        let label = info.label();
+        let proto = info.proto();
+        let flags = info.flags();
+        let utcb = unsafe { UTCB::get() };
+        let msg = utcb.mrs_regs;
+
         log!(
             "Received message: badge={}, label={}, proto={}, flags={}, msg={:?}",
             badge,
@@ -86,56 +82,48 @@ impl SystemService for InitManager {
         if proto != protocol::INIT_PROTO {
             return Err(Error::InvalidProtocol);
         }
-        let utcb = unsafe { UTCB::get() };
         match label {
             protocol::init::START => {
                 let name =
                     unsafe { utcb.read_postcard::<String>().map_err(|_| Error::InvalidParam) }?;
                 self.start_service(name)?;
-                Ok([0; 8])
+                Ok(())
             }
             protocol::init::STOP => {
                 let name =
                     unsafe { utcb.read_postcard::<String>().map_err(|_| Error::InvalidParam) }?;
                 self.stop_service(name)?;
-                Ok([0; 8])
+                Ok(())
             }
             protocol::init::RESTART => {
                 let name =
                     unsafe { utcb.read_postcard::<String>().map_err(|_| Error::InvalidParam) }?;
                 self.restart_service(name)?;
-                Ok([0; 8])
+                Ok(())
             }
             protocol::init::RELOAD => {
                 let name =
                     unsafe { utcb.read_postcard::<String>().map_err(|_| Error::InvalidParam) }?;
                 self.reload_service(name)?;
-                Ok([0; 8])
+                Ok(())
             }
             protocol::init::QUERY => {
                 let name =
                     unsafe { utcb.read_postcard::<String>().map_err(|_| Error::InvalidParam) }?;
                 let status = self.query_service(name)?;
                 (unsafe { utcb.write_postcard(&status).map_err(|_| Error::InvalidParam) })?;
-                Ok([0, 0, 0, 0, 0, 0, 0, 0])
+                Ok(())
             }
             protocol::init::LIST => {
                 let services = self.list_services()?;
                 (unsafe { utcb.write_postcard(&services).map_err(|_| Error::InvalidParam) })?;
-                Ok([0, 0, 0, 0, 0, 0, 0, 0])
+                Ok(())
             }
             _ => Err(Error::NotImplemented),
         }
     }
-    fn reply(
-        &mut self,
-        label: usize,
-        proto: usize,
-        flags: MsgFlags,
-        msg: MsgArgs,
-    ) -> Result<(), Error> {
-        let tag = MsgTag::new(proto, label, flags);
-        self.reply.reply(tag, msg)
+    fn reply(&mut self, info: MsgTag) -> Result<(), Error> {
+        self.reply.reply(info)
     }
     fn stop(&mut self) {
         self.running = false;
